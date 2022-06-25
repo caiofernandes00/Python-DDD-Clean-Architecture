76716dd3d2ad1cb1010092eb6c5486dfab944973
Caio <caiow.wk@gmail.com>
2022-06-25 00:44:49 -0300

    test: add order_item suit test

--- src/entity/order_item.py
+++ src/entity/order_item.py
@@ -4,6 +4,7 @@ class OrderItem:
         self.__id = uid
         self.__name = name
         self.__price = price
+        self.validate()
 
     @property
     def id(self) -> str:
@@ -16,3 +17,20 @@ class OrderItem:
     @property
     def price(self) -> float:
         return self.__price
+
+    def validate(self):
+        self.__validate_id()
+        self.__validate_name()
+        self.__validate_price()
+
+    def __validate_id(self) -> None:
+        if self.__id.__len__() == 0:
+            raise Exception("id is required")
+
+    def __validate_name(self):
+        if self.__name.__len__() == 0:
+            raise Exception("name is required")
+
+    def __validate_price(self):
+        if self.__price < 0:
+            raise Exception("price cannot be negative")
